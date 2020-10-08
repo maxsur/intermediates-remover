ca5451061dcb11784a571d4ad9f10d5a1ca4a384
Суриков Максим <ms@itsbel.by>
2020-10-08 20:30:55 +0300

    IntermediatesRemoverCli added

---
+++ IntermediatesRemover/IntermediatesRemoverCli/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IntermediatesRemoverCli
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string defaultFolderNames = "bin,obj,Debug,Release,DebugUnitTests,TestResults,lut";
+            Console.WriteLine($"Hello '{defaultFolderNames}'!");
+        }
+    }
+}
