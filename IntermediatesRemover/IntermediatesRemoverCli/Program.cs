5599a6d57c564d31adc85e112461e48282e0f372
Суриков Максим <ms@itsbel.by>
2020-12-06 00:12:33 +0300

    root folder option

--- IntermediatesRemover/IntermediatesRemoverCli/Program.cs
+++ IntermediatesRemover/IntermediatesRemoverCli/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading;
+using CommandLine;
 using IntermediatesRemoverCommon;
+using JetBrains.Annotations;
 
 namespace IntermediatesRemoverCli
 {
@@ -11,8 +13,11 @@ namespace IntermediatesRemoverCli
     {
         private static void Main(string[] args)
         {
-            const string defaultRootName = @"C:\Projects";
-            
+            CliOptions options = new CliOptions();
+            Parser.Default.ParseArguments<CliOptions>(args).WithParsed(opts => options = opts);
+
+            Console.WriteLine($"Root folder: {options.RootFolderName}");
+
             // Should be case sensitive
             const string defaultFolderNames = "bin,obj,Debug,Release,DebugUnitTests,TestResults,lut";
             
@@ -24,7 +29,7 @@ namespace IntermediatesRemoverCli
 
             var folders = new Folders(source.Token);
             folders.FindProgressChanged += (sender, eventArgs) => foundFolderNames.Add(eventArgs.Folder);
-            folders.FindFolders(defaultRootName, defaultFolderNames.Split(','));
+            folders.FindFolders(options.RootFolderName, defaultFolderNames.Split(','));
 
             var foundFolders = foundFolderNames.Select(folderName => new Folder(folderName));
             foreach (var folder in foundFolders) folder.Delete();
@@ -35,5 +40,13 @@ namespace IntermediatesRemoverCli
                 Console.ReadKey();
             }
         }
+    }
+
+    /// <summary> CLI options. </summary>
+    [UsedImplicitly]
+    public sealed class CliOptions
+    {
+        [Option('r', "root", Required = true, HelpText = "Root folder with Visual Studio projects.")]
+        public string RootFolderName { get; set; }
     }
 }
