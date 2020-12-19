12c01edfec593472ec6cd3c795a74d77bc03d06b
Суриков Максим <ms@itsbel.by>
2020-12-19 10:54:24 +0300

    Run.bat "Run as administrator" fix

--- IntermediatesRemover/IntermediatesRemoverCli/Run.bat
+++ IntermediatesRemover/IntermediatesRemoverCli/Run.bat
@@ -1,2 +1,7 @@
-IntermediatesRemoverCli.exe -r "C:\Projects"
+@REM Set current directory if started "Run as administrator"
+@setlocal enableextensions
+@cd /d "%~dp0"
+
+@REM Call Utility
+.\IntermediatesRemoverCli.exe -r "C:\Projects"
 PAUSE
